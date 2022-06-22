52258ffd1e8ed8064e2d60930da4862049f80429
Nhan0504 <ttn0504ltt@gmail.com>
2022-06-22 11:05:24 +0700

    Change menu to grid-list and add a card component

--- Angular/conFusion/src/app/menu/menu.component.ts
+++ Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,6 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+const DISHES: Dish[] = [
+  {
+    id: '0',
+    name: 'Pancake',
+    image: '/assets/images/pancake.jpg',
+    category: 'appetizer',
+    featured: false,
+    label: '',
+    price: '1.99',
+    description: 'Pancake drizzel with honey'
+  },
+  {
+    id: '1',
+    name: 'Shrimp',
+    image: '/assets/images/shrimp.jpg',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Sear shrimp cook with paprika and butter'
+  },
+  {
+    id: '2',
+    name: 'Pho Tron',
+    image: '/assets/images/pho.jpg',
+    category: 'main',
+    featured: true,
+    label: '',
+    price: '5.99',
+    description: 'Pho noodle with stir fry beef, peanut, and herbs along with special sauce'
+  },
+  {
+    id: '3',
+    name: 'Tuna',
+    image: '/assets/images/tuna.jpg',
+    category: 'main',
+    featured: true,
+    label: '',
+    price: '10.99',
+    description: 'Medium rare tuna steak with creamy butter and lemon sauce'
+  },
+  {
+    id: '4',
+    name: 'Matcha Cake',
+    image: '/assets/images/matchaCake.jpg',
+    category: 'dessert',
+    featured: false,
+    label: '',
+    price: '2.99',
+    description: 'A delectable, soft sponge cake and Matcha cream, with a little bitterness from Chocolate glazing'
+  }
+] 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,59 +62,9 @@ import { Dish } from '../shared/dish';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = [
-    {
-      id: '0',
-      name: 'Pancake',
-      image: '/assets/images/pancake.jpg',
-      category: 'appetizer',
-      featured: false,
-      label: '',
-      price: '1.99',
-      description: 'Pancake drizzel with honey'
-    },
-    {
-      id: '1',
-      name: 'Shrimp',
-      image: '/assets/images/shrimp.jpg',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      price: '4.99',
-      // tslint:disable-next-line:max-line-length
-      description: 'Sear shrimp cook with paprika and butter'
-    },
-    {
-      id: '2',
-      name: 'Pho Tron',
-      image: '/assets/images/pho.jpg',
-      category: 'main',
-      featured: true,
-      label: '',
-      price: '5.99',
-      description: 'Pho noodle with stir fry beef, peanut, and herbs along with special sauce'
-    },
-    {
-      id: '3',
-      name: 'Tuna',
-      image: '/assets/images/tuna.jpg',
-      category: 'main',
-      featured: true,
-      label: '',
-      price: '10.99',
-      description: 'Medium rare tuna steak with creamy butter and lemon sauce'
-    },
-    {
-      id: '4',
-      name: 'Matcha Cake',
-      image: '/assets/images/matchaCake.jpg',
-      category: 'dessert',
-      featured: false,
-      label: '',
-      price: '2.99',
-      description: 'A delectable, soft sponge cake and Matcha cream, with a little bitterness from Chocolate glazing'
-    }
-  ];
+  dishes = DISHES;
+
+  selectedDish: Dish = DISHES[3];
 
   constructor() { }
 
