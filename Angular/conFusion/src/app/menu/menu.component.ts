7c2a21b9f5eb79d1597ea640426e34d45da22469
Nhan0504 <ttn0504ltt@gmail.com>
2022-06-22 22:28:04 +0700

    Refactor code to share the dishes file

--- Angular/conFusion/src/app/menu/menu.component.ts
+++ Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,49 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 
-const DISHES: Dish[] = [
-  {
-    id: '0',
-    name: 'Shrimp',
-    image: '/assets/images/shrimp.jpg',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'Sear shrimp cook with paprika and butter'
-  },
-  {
-    id: '1',
-    name: 'Pho Tron',
-    image: '/assets/images/pho.jpg',
-    category: 'main',
-    featured: true,
-    label: '',
-    price: '5.99',
-    description: 'Pho noodle with stir fry beef, peanut, and herbs along with special sauce'
-  },
-  {
-    id: '2',
-    name: 'Tuna',
-    image: '/assets/images/tuna.jpg',
-    category: 'main',
-    featured: true,
-    label: '',
-    price: '10.99',
-    description: 'Medium rare tuna steak with creamy butter and lemon sauce'
-  },
-  {
-    id: '3',
-    name: 'Matcha Cake',
-    image: '/assets/images/matchaCake.jpg',
-    category: 'dessert',
-    featured: false,
-    label: '',
-    price: '2.99',
-    description: 'A delectable, soft sponge cake and Matcha cream, with a little bitterness from Chocolate glazing'
-  }
-] 
 
 @Component({
   selector: 'app-menu',
