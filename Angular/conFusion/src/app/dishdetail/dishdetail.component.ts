0f19d18b13480d5e4c623664e87c9e5fdf6763cd
Nhan0504 <ttn0504ltt@gmail.com>
2022-06-25 16:47:13 +0700

    Get the dish id from the URL send by the menu component

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
-
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-dishdetail',
@@ -10,12 +11,19 @@ import { DISHES } from '../shared/dishes';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
   dish!: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
