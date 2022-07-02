e6e7fdfaccc1d8257cf7d8fd8b3004f4ef0a64d7
Nhan0504 <ttn0504ltt@gmail.com>
2022-07-02 18:11:48 +0700

    Return the first value emits by the Observable

--- Angular/conFusion/src/app/services/dish.service.ts
+++ Angular/conFusion/src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { firstValueFrom, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +12,15 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(DISHES), 2000)
-    });
+    return firstValueFrom(of(DISHES).pipe(delay(2000)));
   }
 
   getDish(id: string): Promise<Dish> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000)
-    });
+    return firstValueFrom(of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000)));
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000)
-    });
+    return firstValueFrom(of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000)));
   }
   
 }
--- Angular/conFusion/src/app/services/promotion.service.ts
+++ Angular/conFusion/src/app/services/promotion.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { firstValueFrom, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,20 +12,14 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS), 2000)
-    });
+    return firstValueFrom(of(PROMOTIONS).pipe(delay(2000)));
   }
 
   getPromotion(id: string): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000)
-    });
+    return firstValueFrom(of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000)));
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]), 2000)
-    });
+    return firstValueFrom(of(PROMOTIONS.filter((promo) => promo.featured)[0]).pipe(delay(2000)));
   }
 }
