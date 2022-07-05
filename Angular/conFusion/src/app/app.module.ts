4aaae7b82eec86bd8cfdffa6e9623d34fbaacbeb
Nhan0504 <ttn0504ltt@gmail.com>
2022-07-05 18:02:33 +0700

    Add slider module for user to add rating

--- Angular/conFusion/src/app/app.module.ts
+++ Angular/conFusion/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -70,6 +71,7 @@ import { baseURL } from './shared/baseurl';
     MatSelectModule,
     MatSlideToggleModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
     HttpClientModule
   ],
   providers: [
