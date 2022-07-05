4ee9ba28656d6ea79e07426f5b11ddae3e4c2832
Nhan0504 <ttn0504ltt@gmail.com>
2022-07-05 15:42:30 +0700

    Use static web service to serve the images in the menu

--- Angular/conFusion/src/app/menu/menu.component.ts
+++ Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
@@ -12,9 +12,8 @@ export class MenuComponent implements OnInit {
 
   dishes!: Dish[];
 
-  selectedDish!: Dish;
-
-  constructor(private dishService: DishService) { }
+  constructor(private dishService: DishService,
+    @Inject('baseURL') public baseURL: string) { }
 
   ngOnInit(): void {
     this.dishService.getDishes()
