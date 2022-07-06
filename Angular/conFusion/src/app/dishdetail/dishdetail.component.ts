7e3ca6091802ef754cda9a2fc60ae34eb4b3790b
Nhan0504 <ttn0504ltt@gmail.com>
2022-07-06 15:49:08 +0700

    Add animation between transition from a dish to another dish in the disdetail

--- Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,25 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
 import { switchMap } from 'rxjs';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+          transform: 'scale(1.0)',
+          opacity: 1
+      })),
+      state('hidden', style({
+          transform: 'scale(0.5)',
+          opacity: 0.2
+      })),
+      transition('* => *', animate('1s ease-in-out'))
+  ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -23,6 +37,7 @@ export class DishdetailComponent implements OnInit {
   @ViewChild('cform') commentFormDirective: any;
   comment!: Comment;
   commentForm!: FormGroup;
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -52,9 +67,9 @@ export class DishdetailComponent implements OnInit {
     this.dishService.getDishIds()
       .subscribe(dishIds => this.dishIds = dishIds);
     let id = this.route.params
-      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+      .pipe(switchMap((params: Params) => {this.visibility = 'hidden'; return this.dishService.getDish(params['id'])}))
       .subscribe({
-        next: dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+        next: dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
         error: errMess => this.errMess = <any>errMess
       });
   }
