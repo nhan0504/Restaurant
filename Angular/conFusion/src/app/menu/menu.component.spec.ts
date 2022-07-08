b4133b4d9fea0785ed2d74eb86b3243529f735cc
Nhan0504 <ttn0504ltt@gmail.com>
2022-07-08 15:34:19 +0700

    Check if the values in the component file are correct

--- Angular/conFusion/src/app/menu/menu.component.spec.ts
+++ Angular/conFusion/src/app/menu/menu.component.spec.ts
@@ -1,16 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { DISHES } from '../shared/dishes';
+import { baseURL } from '../shared/baseurl';
+import { Observable, of } from 'rxjs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async () => {
+    const dishServiceStub = {
+      getDishes: function(): Observable<Dish[]> {
+        return of(DISHES);
+      }
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [ BrowserAnimationsModule,
+        FlexLayoutModule,
+        MatGridListModule,
+        MatProgressSpinnerModule,
+        RouterTestingModule.withRoutes([{ path: 'menu', component: MenuComponent }])
+      ],
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'baseURL', useValue: baseURL },
+      ]
     })
     .compileComponents();
+
+    const dishservice = TestBed.inject(DishService);
   });
 
   beforeEach(() => {
@@ -22,4 +52,11 @@ describe('MenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('dishes items should be 4',() => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Pho Tron');
+    expect(component.dishes[3].featured).toBeFalsy;
+  });
+
 });
