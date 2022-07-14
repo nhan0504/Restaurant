a283438c0ea029236245e2a74919aaa0f45f1315
Nhan0504 <ttn0504ltt@gmail.com>
2022-07-14 11:38:45 +0700

    Add REST end point for /leaders

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter')
+const leaderRouter = require('./routes/leaderRouter')
 
 const hostname = 'localhost';
 const port = 3000;
@@ -14,10 +15,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
